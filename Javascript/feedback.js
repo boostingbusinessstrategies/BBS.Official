c8522ff47078a10c4f296ca4a17e21c37db40019
boostingbusinessstrategies <musicloverdudelol@gmail.com>
2024-10-08 00:58:25 -0600

    Update feedback.js

--- Javascript/feedback.js
+++ Javascript/feedback.js
@@ -206,29 +206,21 @@ document.addEventListener('DOMContentLoaded', function() {
     updateAdminControlsVisibility();
 });
 
-// Obtén referencias a los elementos
+// Get references to elements
 const sidebar = document.getElementById('sidebar');
 const menuToggle = document.getElementById('menu-toggle');
 
-// Añade un event listener al botón del menú
+// Add an event listener to the menu button
 menuToggle.addEventListener('click', () => {
-    // Alterna la clase 'active' en el sidebar
+    // Toggle the 'active' class on the sidebar
     sidebar.classList.toggle('active');
 });
 
-// Añade un event listener al documento para detectar clics fuera del sidebar
+// Add an event listener to the document to detect clicks outside the sidebar
 document.addEventListener('click', (event) => {
-    // Si el sidebar está activo y el clic ocurre fuera del sidebar y del menú toggle
+    // If the sidebar is active and the click occurs outside the sidebar and the menu toggle
     if (sidebar.classList.contains('active') && !sidebar.contains(event.target) && !menuToggle.contains(event.target)) {
-        // Elimina la clase 'active' para cerrar el sidebar
+        // Remove the 'active' class to close the sidebar
         sidebar.classList.remove('active');
     }
-});
-
-// Añadir un event listener a los enlaces del sidebar para cerrarlo al hacer clic
-const sidebarLinks = sidebar.querySelectorAll('ul li a');
-sidebarLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        sidebar.classList.remove('active'); // Cierra el sidebar al hacer clic en un enlace
-    });
 });
